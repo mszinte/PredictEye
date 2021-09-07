fix averaging code for cifti

mri_analysis/preproc/preproc_end.py
@@ -18,7 +18,9 @@ To run:
 python preproc/preproc_end.py [subject name] [registration_type]
 -----------------------------------------------------------------------------------------
 Exemple:
-python preproc/preproc_end.py sub-01 MNI152NLin2009cAsym
+python preproc/preproc_end.py sub-01 T1w
+python preproc/preproc_end.py sub-01 MNI152NLin2009cAsym-res-1
+python preproc/preproc_end.py sub-01 fsLR_den-170k
 -----------------------------------------------------------------------------------------
 Written by Martin Szinte (martin.szinte@gmail.com)
 -----------------------------------------------------------------------------------------
@@ -35,18 +37,25 @@ import json
 import sys
 import os
 import glob
-#import ipdb
+import ipdb
 import platform
 import numpy as np
 opj = os.path.join
-#deb = ipdb.set_trace
+deb = ipdb.set_trace
 
 sub_name = sys.argv[1]
 regist_type = sys.argv[2]
 
+if regist_type == 'fsLR_den-170k':
+    file_ends = ['.npy','_subc.npy']
+else: 
+    file_ends = '.nii.gz'
+
+
 # MRI analysis imports
 # --------------------
 import nibabel as nb
+
 from scipy.signal import savgol_filter
 
 with open('settings.json') as f:
@@ -60,60 +69,77 @@ base_dir = analysis_info['base_dir']
 
 # Copy files in pp_data folder
 # ----------------------------
-dest_folder1 = "{base_dir}/pp_data/{sub}/func/fmriprep_dct".format(base_dir = base_dir, sub = sub_name)
+dest_folder1 = "{base_dir}/pp_data_new/{sub}/func/fmriprep_dct".format(base_dir=base_dir, sub=sub_name)
 try: os.makedirs(dest_folder1)
 except: pass
 
-dest_folder2 = "{base_dir}/pp_data/{sub}/func/fmriprep_dct_pca".format(base_dir = base_dir, sub = sub_name)
+dest_folder2 = "{base_dir}/pp_data_new/{sub}/func/fmriprep_dct_pca".format(base_dir=base_dir, sub=sub_name)
 try: os.makedirs(dest_folder2)
 except: pass
 
-orig_folder = "{base_dir}/deriv_data/pybest/{sub}".format(base_dir = base_dir, sub = sub_name)
+orig_folder = "{base_dir}/deriv_data/pybest_new/{sub}".format(base_dir=base_dir, sub=sub_name)
+
+# for task_num, task_name in enumerate(analysis_info['task_names']):
+#     for task_run in np.arange(0,analysis_info['task_runs'][task_num],1):
+#         for ses_num,ses_name in enumerate(next(os.walk(orig_folder))[1]):
+#             for file_end in file_ends:
+#                 # dct func
+
+#                 orig_file1 = "{orig_fold}/{ses}/preproc/{sub}_{ses}_task-{task_name}_run-{task_run}_space-{reg}_desc-preproc_bold{file_end}".format(
+#                                         orig_fold=orig_folder, sub=sub_name, ses=ses_name, task_name=task_name, reg=regist_type, task_run=task_run+1, file_end=file_end)
+#                 dest_file1 = "{dest_fold}/{sub}_task-{task_name}_run-{task_run}_space-{reg}_fmriprep_dct{file_end}".format(
+#                                         dest_fold=dest_folder1, sub=sub_name, task_name=task_name, reg=regist_type, task_run=task_run+1, file_end=file_end)
 
 
-for task_num, task_name in enumerate(analysis_info['task_names']):
-    for task_run in np.arange(0,analysis_info['task_runs'][task_num],1):
-        for ses_num,ses_name in enumerate(next(os.walk(orig_folder))[1]):
-            # dct func
-            orig_file1 = "{orig_fold}/{ses}/preproc/{sub}_{ses}_task-{task_name}_space-{reg}_run-{task_run}_desc-preproc_bold.nii.gz".format(
-                                    orig_fold = orig_folder, sub = sub_name, ses = ses_name, task_name = task_name, reg = regist_type, task_run = task_run+1)
-            dest_file1 = "{dest_fold}/{sub}_task-{task_name}_space-{reg}_run-{task_run}_fmriprep_dct.nii.gz".format(
-                                    dest_fold = dest_folder1, sub = sub_name, task_name = task_name, reg = regist_type, task_run = task_run+1)
+#                 if os.path.isfile(orig_file1):
+#                     os.system("{cmd} {orig} {dest}".format(cmd=trans_cmd, orig=orig_file1, dest=dest_file1))
 
-            if os.path.isfile(orig_file1):
-                os.system("{cmd} {orig} {dest}".format(cmd = trans_cmd, orig = orig_file1, dest = dest_file1))
+#                 # dct + denoised func
+#                 orig_file2 = "{orig_fold}/{ses}/denoising/{sub}_{ses}_task-{task_name}_run-{task_run}_space-{reg}_desc-denoised_bold{file_end}".format(
+#                                         orig_fold=orig_folder, sub=sub_name, ses=ses_name, task_name=task_name, reg=regist_type, task_run=task_run+1, file_end=file_end)
+#                 dest_file2 = "{dest_fold}/{sub}_task-{task_name}_run-{task_run}_space-{reg}_fmriprep_dct_pca{file_end}".format(
+#                                         dest_fold=dest_folder2, sub=sub_name, task_name=task_name, reg=regist_type, task_run=task_run+1, file_end=file_end)
 
-            # dct + denoised func
-            orig_file2 = "{orig_fold}/{ses}/denoising/{sub}_{ses}_task-{task_name}_space-{reg}_run-{task_run}_desc-denoised_bold.nii.gz".format(
-                                    orig_fold = orig_folder, sub = sub_name, ses = ses_name, task_name = task_name, reg = regist_type, task_run = task_run+1)
-            dest_file2 = "{dest_fold}/{sub}_task-{task_name}_space-{reg}_run-{task_run}_fmriprep_dct_pca.nii.gz".format(
-                                    dest_fold = dest_folder2, sub = sub_name, task_name = task_name, reg = regist_type, task_run = task_run+1)
-
-            if os.path.isfile(orig_file2):
-                os.system("{cmd} {orig} {dest}".format(cmd = trans_cmd, orig = orig_file2, dest = dest_file2))
+#                 if os.path.isfile(orig_file2):
+#                     os.system("{cmd} {orig} {dest}".format(cmd=trans_cmd, orig=orig_file2, dest=dest_file2))
 
 # Average tasks runs
 for preproc in analysis_info['preproc']:
     for task_name in analysis_info['task_names']:
+        for file_end in file_ends:
+            print('avg: '+task_name+' type: '+file_end)
 
-        file_list = sorted(glob.glob("{base_dir}/pp_data/{sub}/func/{preproc}/*{task_name}_space-{reg}_*.nii.gz".format(
-                                     base_dir = base_dir, sub = sub_name, preproc = preproc,task_name = task_name, reg = regist_type)))
+            file_list = sorted(glob.glob("{base_dir}/pp_data_new/{sub}/func/{preproc}/*{task_name}*_space-{reg}_{preproc}{file_end}".format(
+                                         base_dir=base_dir, sub=sub_name, preproc=preproc,task_name=task_name, reg=regist_type, file_end=file_end)))
+            
+            if len(file_list):
+                
+                # save
+                new_file = "{base_dir}/pp_data_new/{sub}/func/{sub}_task-{task_name}_space-{reg}_{preproc}_avg{file_end}".format(
+                            base_dir=base_dir, sub=sub_name, preproc=preproc, task_name=task_name, reg=regist_type, file_end=file_end)
 
-        img = nb.load(file_list[0])
-        data_avg = np.zeros(img.shape)
 
-        print('avg: '+task_name)
-        for file in file_list:
-            print('add: '+file)
+                if regist_type == 'fsLR_den-170k':
+                    img = np.load(file_list[0])
+                    data_avg = np.zeros(img.shape)
 
-            # load
-            data_psc = []
-            data_psc_img = nb.load(file)
-            data_psc = data_psc_img.get_fdata()
-            data_avg += data_psc/len(file_list)
+                    for file in file_list:
+                        print('add: '+file)
+                        data_val = []
+                        data_val = np.load(file)
+                        data_avg += data_val/len(file_list)
+                    np.save(new_file, data_avg)
 
-            # save
-            new_file = "{base_dir}/pp_data/{sub}/func/{sub}_task-{task_name}_space-{reg}_{preproc}_avg.nii.gz".format(
-                        base_dir = base_dir, sub = sub_name, preproc = preproc, task_name = task_name, reg = regist_type)
-            new_img = nb.Nifti1Image(dataobj = data_avg, affine = img.affine, header = img.header)
-            new_img.to_filename(new_file)
+                else: 
+                    img = nb.load(file_list[0])
+                    data_avg = np.zeros(img.shape)
+
+                    for file in file_list:
+                        print('add: '+file)
+                        data_val = []
+                        data_val_img = nb.load(file)
+                        data_val = data_val_img.get_fdata()
+                        data_avg += data_val/len(file_list)
+
+                    new_img = nb.Nifti1Image(dataobj=data_avg, affine=img.affine, header=img.header)
+                    new_img.to_filename(new_file)